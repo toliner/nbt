841af42e612bc99f830b8cd9a82b45855b1993d4
トリナー <toliner.rkt@gmail.com>
2018-03-04 06:13:30 +0900

    fix TagType.BYTE

--- src/main/kotlin/toliner/nbt/TagType.kt
+++ src/main/kotlin/toliner/nbt/TagType.kt
@@ -5,5 +5,5 @@ import kotlin.reflect.KClass
 
 enum class TagType(val tagClass: KClass<out NBTTag<*>>, val tagName: String, val id: Int) {
     END(NBTTag.End::class, "TAG_END", 0),
-    BYTE(NBTTag::class, "TAG_BYTE", 1)
+    BYTE(NBTTag.Byte::class, "TAG_BYTE", 1)
 }
