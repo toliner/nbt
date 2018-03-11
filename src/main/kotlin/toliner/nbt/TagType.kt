36f800b1db9be1610233805d874e8b00b23bad8a
トリナー <toliner.rkt@gmail.com>
2018-03-11 09:29:22 +0900

    fix tag name

--- src/main/kotlin/toliner/nbt/TagType.kt
+++ src/main/kotlin/toliner/nbt/TagType.kt
@@ -8,7 +8,7 @@ enum class TagType(val tagClass: KClass<out NBTTag<*>>, val tagName: String, val
     BYTE(NBTTag.Byte::class, "TAG_BYTE", 1),
     SHORT(NBTTag.Short::class, "TAG_Short", 2),
     INT(NBTTag.Int::class, "TAG_Int", 3),
-    LONG(NBTTag.Long::class, "TAG_LONG", 4),
-    FLOAT(NBTTag.Float::class, "TAG_FLOAT", 5),
-    DOUBLE(NBTTag.Double::class, "TAG_DOUBLE", 6)
+    LONG(NBTTag.Long::class, "TAG_Long", 4),
+    FLOAT(NBTTag.Float::class, "TAG_Float", 5),
+    DOUBLE(NBTTag.Double::class, "TAG_Double", 6)
 }
