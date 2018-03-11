4519f9f87c0c1284fff561813f08fa8cf13947fe
トリナー <toliner.rkt@gmail.com>
2018-03-11 10:26:25 +0900

    add TagType.String

--- src/main/kotlin/toliner/nbt/TagType.kt
+++ src/main/kotlin/toliner/nbt/TagType.kt
@@ -11,5 +11,6 @@ enum class TagType(val tagClass: KClass<out NBTTag<*>>, val tagName: String, val
     LONG(NBTTag.Long::class, "TAG_Long", 4),
     FLOAT(NBTTag.Float::class, "TAG_Float", 5),
     DOUBLE(NBTTag.Double::class, "TAG_Double", 6),
-    BYTE_ARRAY(NBTTag.ByteArray::class, "TAG_Byte_Array", 7)
+    BYTE_ARRAY(NBTTag.ByteArray::class, "TAG_Byte_Array", 7),
+    STRING(NBTTag.String::class, "TAG_String", 8)
 }
--- src/main/kotlin/toliner/nbt/tags/NBTTag.kt
+++ src/main/kotlin/toliner/nbt/tags/NBTTag.kt
@@ -6,7 +6,7 @@ private typealias Integer = kotlin.Int
 
 sealed class NBTTag<out T>(val tagType: TagType) : Comparable<NBTTag<*>> {
     abstract val value: T
-    abstract val name: String
+    abstract val name: kotlin.String
     abstract fun clone(): NBTTag<T>
 
     override fun compareTo(other: NBTTag<*>): Integer {
@@ -22,7 +22,7 @@ sealed class NBTTag<out T>(val tagType: TagType) : Comparable<NBTTag<*>> {
     }
 
     class End : NBTTag<Any?>(TagType.END) {
-        override val name: String
+        override val name: kotlin.String
             get() = ""
 
         override val value: Any?
@@ -33,47 +33,53 @@ sealed class NBTTag<out T>(val tagType: TagType) : Comparable<NBTTag<*>> {
         }
     }
 
-    data class Byte(override val name: String, override val value: kotlin.Byte) : NBTTag<kotlin.Byte>(TagType.BYTE) {
+    data class Byte(override val name: kotlin.String, override val value: kotlin.Byte) : NBTTag<kotlin.Byte>(TagType.BYTE) {
         override fun clone(): NBTTag<kotlin.Byte> {
             return copy()
         }
     }
 
-    data class Short(override val name: String, override val value: kotlin.Short) : NBTTag<kotlin.Short>(TagType.SHORT) {
+    data class Short(override val name: kotlin.String, override val value: kotlin.Short) : NBTTag<kotlin.Short>(TagType.SHORT) {
         override fun clone(): NBTTag<kotlin.Short> {
             return copy()
         }
     }
 
-    data class Int(override val name: String, override val value: Integer) : NBTTag<Integer>(TagType.INT) {
+    data class Int(override val name: kotlin.String, override val value: Integer) : NBTTag<Integer>(TagType.INT) {
         override fun clone(): NBTTag<Integer> {
             return copy()
         }
     }
 
-    data class Long(override val name: String, override val value: kotlin.Long) : NBTTag<kotlin.Long>(TagType.LONG) {
+    data class Long(override val name: kotlin.String, override val value: kotlin.Long) : NBTTag<kotlin.Long>(TagType.LONG) {
         override fun clone(): NBTTag<kotlin.Long> {
             return copy()
         }
     }
 
-    data class Float(override val name: String, override val value: kotlin.Float) : NBTTag<kotlin.Float>(TagType.FLOAT) {
+    data class Float(override val name: kotlin.String, override val value: kotlin.Float) : NBTTag<kotlin.Float>(TagType.FLOAT) {
         override fun clone(): NBTTag<kotlin.Float> {
             return copy()
         }
     }
 
-    data class Double(override val name: String, override val value: kotlin.Double) : NBTTag<kotlin.Double>(TagType.DOUBLE) {
+    data class Double(override val name: kotlin.String, override val value: kotlin.Double) : NBTTag<kotlin.Double>(TagType.DOUBLE) {
         override fun clone(): NBTTag<kotlin.Double> {
             return copy()
         }
     }
 
-    data class ByteArray(override val name: String, private val list: List<kotlin.Byte>) : NBTTag<kotlin.ByteArray>(TagType.BYTE_ARRAY) {
+    data class ByteArray(override val name: kotlin.String, private val list: List<kotlin.Byte>) : NBTTag<kotlin.ByteArray>(TagType.BYTE_ARRAY) {
         override val value: kotlin.ByteArray
             get() = list.toByteArray()
 
         override fun clone(): NBTTag<kotlin.ByteArray> {
+            return copy()
+        }
+    }
+
+    data class String(override val name: kotlin.String, override val value: kotlin.String) : NBTTag<kotlin.String>(TagType.STRING) {
+        override fun clone(): NBTTag<kotlin.String> {
             return copy()
         }
     }
