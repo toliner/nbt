c7a823e4edfe126b08ddc27850e37abac0f9421f
トリナー <toliner.rkt@gmail.com>
2018-03-24 18:33:14 +0900

    rename NBTTag class to NBTTags, and impl NBTTag

--- src/main/kotlin/toliner/nbt/NBTTags.kt
+++ src/main/kotlin/toliner/nbt/NBTTags.kt
@@ -13,24 +13,17 @@ import kotlin.reflect.KClass
 private typealias Integer = kotlin.Int
 
 /**
- * This class is base class of all tag classes.
- * All subclasses are nested under this class because all tag types are pre-defined.
+ * This class is base class of all **immutable** tag classes.
+ * This class is reference impl for [NBTTag].
  * This is comparable by [name] for sorting tags.
  *
  * @property tagType value of [TagType] for the tag.
  * @property value data that the tag has.
  * @property name name of the tag. It will be used for identifying tag.
  */
-sealed class NBTTagBase<out T>(val tagType: TagType) : Comparable<NBTTagBase<*>> {
-    abstract val value: T
-    abstract val name: kotlin.String
+sealed class NBTTags<out T>(val tagType: TagType) : NBTTag<T> {
 
-    /**
-     * To use create same name and value tag.
-     */
-    abstract fun clone(): NBTTagBase<T>
-
-    override fun compareTo(other: NBTTagBase<*>): Integer {
+    override fun compareTo(other: NBTTag<*>): kotlin.Int {
         return if (this == other) {
             0
         } else {
@@ -46,14 +39,14 @@ sealed class NBTTagBase<out T>(val tagType: TagType) : Comparable<NBTTagBase<*>>
      * The [TagType.END] tag.
      * This tag indicates end of [TagType.COMPOUND].
      */
-    class End : NBTTagBase<Any?>(TagType.END) {
+    class End : NBTTags<Nothing?>(TagType.END) {
         override val name: kotlin.String
             get() = ""
 
-        override val value: Any?
+        override val value: Nothing?
             get() = null
 
-        override fun clone(): NBTTagBase<Any?> {
+        override fun clone(): NBTTags<Nothing?> {
             return End()
         }
     }
@@ -62,8 +55,8 @@ sealed class NBTTagBase<out T>(val tagType: TagType) : Comparable<NBTTagBase<*>>
      * The [TagType.BYTE] tag.
      * This tag stores one-byte-signed-integer data.
      */
-    data class Byte(override val name: kotlin.String, override val value: kotlin.Byte) : NBTTagBase<kotlin.Byte>(TagType.BYTE) {
-        override fun clone(): NBTTagBase<kotlin.Byte> {
+    data class Byte(override val name: kotlin.String, override val value: kotlin.Byte) : NBTTags<kotlin.Byte>(TagType.BYTE) {
+        override fun clone(): NBTTags<kotlin.Byte> {
             return copy()
         }
     }
@@ -72,8 +65,8 @@ sealed class NBTTagBase<out T>(val tagType: TagType) : Comparable<NBTTagBase<*>>
      * The [TagType.SHORT] tag.
      * This tag stores two-bytes-signed-integer data, which we use it as [kotlin.Short]
      */
-    data class Short(override val name: kotlin.String, override val value: kotlin.Short) : NBTTagBase<kotlin.Short>(TagType.SHORT) {
-        override fun clone(): NBTTagBase<kotlin.Short> {
+    data class Short(override val name: kotlin.String, override val value: kotlin.Short) : NBTTags<kotlin.Short>(TagType.SHORT) {
+        override fun clone(): NBTTags<kotlin.Short> {
             return copy()
         }
     }
@@ -82,8 +75,8 @@ sealed class NBTTagBase<out T>(val tagType: TagType) : Comparable<NBTTagBase<*>>
      * The [TagType.INT] tag.
      * This tag stores four-bytes-signed-integer data, which we use it as [kotlin.Int]
      */
-    data class Int(override val name: kotlin.String, override val value: Integer) : NBTTagBase<Integer>(TagType.INT) {
-        override fun clone(): NBTTagBase<Integer> {
+    data class Int(override val name: kotlin.String, override val value: Integer) : NBTTags<Integer>(TagType.INT) {
+        override fun clone(): NBTTags<Integer> {
             return copy()
         }
     }
@@ -92,8 +85,8 @@ sealed class NBTTagBase<out T>(val tagType: TagType) : Comparable<NBTTagBase<*>>
      * The [TagType.LONG] tag.
      * This tag stores eight-bytes-signed-integer data, which we use it as [kotlin.Long]
      */
-    data class Long(override val name: kotlin.String, override val value: kotlin.Long) : NBTTagBase<kotlin.Long>(TagType.LONG) {
-        override fun clone(): NBTTagBase<kotlin.Long> {
+    data class Long(override val name: kotlin.String, override val value: kotlin.Long) : NBTTags<kotlin.Long>(TagType.LONG) {
+        override fun clone(): NBTTags<kotlin.Long> {
             return copy()
         }
     }
@@ -102,8 +95,8 @@ sealed class NBTTagBase<out T>(val tagType: TagType) : Comparable<NBTTagBase<*>>
      * The [TagType.FLOAT] tag.
      * This tag stores single-precision-floating-point-number data, which we use it as [kotlin.Float]
      */
-    data class Float(override val name: kotlin.String, override val value: kotlin.Float) : NBTTagBase<kotlin.Float>(TagType.FLOAT) {
-        override fun clone(): NBTTagBase<kotlin.Float> {
+    data class Float(override val name: kotlin.String, override val value: kotlin.Float) : NBTTags<kotlin.Float>(TagType.FLOAT) {
+        override fun clone(): NBTTags<kotlin.Float> {
             return copy()
         }
     }
@@ -112,8 +105,8 @@ sealed class NBTTagBase<out T>(val tagType: TagType) : Comparable<NBTTagBase<*>>
      * The [TagType.DOUBLE] tag.
      * This tag stores double-precision-floating-point-number data, which we use it as [kotlin.Double]
      */
-    data class Double(override val name: kotlin.String, override val value: kotlin.Double) : NBTTagBase<kotlin.Double>(TagType.DOUBLE) {
-        override fun clone(): NBTTagBase<kotlin.Double> {
+    data class Double(override val name: kotlin.String, override val value: kotlin.Double) : NBTTags<kotlin.Double>(TagType.DOUBLE) {
+        override fun clone(): NBTTags<kotlin.Double> {
             return copy()
         }
     }
@@ -122,11 +115,11 @@ sealed class NBTTagBase<out T>(val tagType: TagType) : Comparable<NBTTagBase<*>>
      * The [TagType.BYTE_ARRAY] tag.
      * This tag stores an array of Bytes, which we use it as [kotlin.ByteArray]
      */
-    data class ByteArray(override val name: kotlin.String, private val list: kotlin.collections.List<kotlin.Byte>) : NBTTagBase<kotlin.ByteArray>(TagType.BYTE_ARRAY) {
+    data class ByteArray(override val name: kotlin.String, private val list: kotlin.collections.List<kotlin.Byte>) : NBTTags<kotlin.ByteArray>(TagType.BYTE_ARRAY) {
         override val value: kotlin.ByteArray
             get() = list.toByteArray()
 
-        override fun clone(): NBTTagBase<kotlin.ByteArray> {
+        override fun clone(): NBTTags<kotlin.ByteArray> {
             return copy()
         }
     }
@@ -135,28 +128,28 @@ sealed class NBTTagBase<out T>(val tagType: TagType) : Comparable<NBTTagBase<*>>
      * The [TagType.STRING] tag.
      * This tag stores [kotlin.String] data.
      */
-    data class String(override val name: kotlin.String, override val value: kotlin.String) : NBTTagBase<kotlin.String>(TagType.STRING) {
-        override fun clone(): NBTTagBase<kotlin.String> {
+    data class String(override val name: kotlin.String, override val value: kotlin.String) : NBTTags<kotlin.String>(TagType.STRING) {
+        override fun clone(): NBTTags<kotlin.String> {
             return copy()
         }
     }
 
     /**
      * The [TagType.LIST] tag.
-     * This tag stores list of [NBTTagBase]s.
+     * This tag stores list of [NBTTags]s.
      */
-    data class List<T : NBTTagBase<*>>(override val name: kotlin.String, val type: KClass<T>, override val value: kotlin.collections.List<T>) : NBTTagBase<kotlin.collections.List<T>>(TagType.LIST) {
-        override fun clone(): NBTTagBase<kotlin.collections.List<T>> {
+    data class List<T : NBTTags<*>>(override val name: kotlin.String, val type: KClass<T>, override val value: kotlin.collections.List<T>) : NBTTags<kotlin.collections.List<T>>(TagType.LIST) {
+        override fun clone(): NBTTags<kotlin.collections.List<T>> {
             return copy()
         }
     }
 
     /**
      * The [TagType.COMPOUND] tag.
-     * This tag stores some [NBTTagBase]s as key-value map
+     * This tag stores some [NBTTags]s as key-value map
      */
-    data class Compound(override val name: kotlin.String, override val value: CompoundMap) : NBTTagBase<CompoundMap>(TagType.COMPOUND) {
-        override fun clone(): NBTTagBase<CompoundMap> {
+    data class Compound(override val name: kotlin.String, override val value: CompoundMap) : NBTTags<CompoundMap>(TagType.COMPOUND) {
+        override fun clone(): NBTTags<CompoundMap> {
             return copy()
         }
     }
@@ -165,11 +158,11 @@ sealed class NBTTagBase<out T>(val tagType: TagType) : Comparable<NBTTagBase<*>>
      * The [TagType.INT_ARRAY] tag.
      * This tag stores array of [kotlin.Int], which we use it as [kotlin.IntArray]
      */
-    data class IntArray(override val name: kotlin.String, private val list: kotlin.collections.List<kotlin.Int>) : NBTTagBase<kotlin.IntArray>(TagType.INT_ARRAY) {
+    data class IntArray(override val name: kotlin.String, private val list: kotlin.collections.List<kotlin.Int>) : NBTTags<kotlin.IntArray>(TagType.INT_ARRAY) {
         override val value: kotlin.IntArray
             get() = list.toIntArray()
 
-        override fun clone(): NBTTagBase<kotlin.IntArray> {
+        override fun clone(): NBTTags<kotlin.IntArray> {
             return copy()
         }
     }
@@ -179,11 +172,11 @@ sealed class NBTTagBase<out T>(val tagType: TagType) : Comparable<NBTTagBase<*>>
      * **WARNING** : This is optional tag. This tag is not defined original NBT format.
      * This tag stores array of [kotlin.Short], which we use it as [kotlin.ShortArray]
      */
-    data class ShortArray(override val name: kotlin.String, private val list: kotlin.collections.List<kotlin.Short>) : NBTTagBase<kotlin.ShortArray>(TagType.SHORT_ARRAY) {
+    data class ShortArray(override val name: kotlin.String, private val list: kotlin.collections.List<kotlin.Short>) : NBTTags<kotlin.ShortArray>(TagType.SHORT_ARRAY) {
         override val value: kotlin.ShortArray
             get() = list.toShortArray()
 
-        override fun clone(): NBTTagBase<kotlin.ShortArray> {
+        override fun clone(): NBTTags<kotlin.ShortArray> {
             return copy()
         }
     }
