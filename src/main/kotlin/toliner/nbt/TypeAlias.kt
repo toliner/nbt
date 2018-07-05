f2bc611e8f4ae8a867bbeaae166f3476bb39fe2d
トリナー <toliner.rkt@gmail.com>
2018-07-06 01:56:35 +0900

    TypeAliasの追加

---
+++ src/main/kotlin/toliner/nbt/TypeAlias.kt
@@ -0,0 +1,15 @@
+package toliner.nbt
+
+typealias TagEnd = NBTTags.End
+typealias TagByte = NBTTags.Byte
+typealias TagShort = NBTTags.Short
+typealias TagInt = NBTTags.Int
+typealias TagLong = NBTTags.Long
+typealias TagFloat = NBTTags.Float
+typealias TagDouble = NBTTags.Double
+typealias TagByteArray = NBTTags.ByteArray
+typealias TagString = NBTTags.String
+typealias TagList<T> = NBTTags.List<T>
+typealias TagCompound = NBTTags.Compound
+typealias TagIntArray = NBTTags.IntArray
+typealias TagShortArray = NBTTags.ShortArray
